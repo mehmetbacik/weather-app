dbe3cba620ae4ec79bdd00f42908293a4ea32557
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-04-05 23:44:54 +0300

    Update
    Updated components.

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import  Cities  from './Cities'
+import Days from './Days'
+function Container() {
+  return (
+    <div>
+      <Cities/>
+      <Days/>
+    </div>
+  )
+}
+
+export default Container
