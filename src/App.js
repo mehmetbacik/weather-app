b509fa0bbbb64d6b29f9f51527246780aac9c2ca
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-04-05 23:30:20 +0300

    Update
    Remove tailwind css.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <h2 className='text-center text-red-600'>Test</h2>
+          <h2>Test</h2>
         </p>
         <a
           className="App-link"
